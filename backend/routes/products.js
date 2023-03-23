f21830074854da0c8f9bf28d2defd351d01e69ed
Melvin Kempe <k3mp314@gmail.com>
2023-03-23 15:21:35 +0100

    [added] code to make token a must to work

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -16,22 +16,24 @@ router.get("/:productId", async(req, res) => {
 })
 
 router.post("/add", async(req, res) => {
-    let newProduct = await productModel.create({
-      name: req.body.name,
-      description: req.body.description,
-      price: req.body.price,
-      stock: req.body.stock,
-      category: req.body.category
-    });
-
-    if(newProduct.name) {
-      console.log("name", newProduct.name);
-      console.log("stock", newProduct.stock);
-    }
-    console.log(newProduct);
+  let token  = req.body.token
+  console.log("token", token)
   
-    console.log("newProduct", newProduct);
-    res.status(201).json(newProduct);
+  if(token === process.env.TOKEN_URI) {
+      let newProduct = await productModel.create({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        stock: req.body.stock,
+        category: req.body.category
+      });
+      console.log(newProduct);
+    
+      console.log("newProduct", newProduct);
+      res.status(201).json(newProduct);
+  } else {
+      res.status(401).json("unauthorised, wrong token")
+  }
   })
   
 
